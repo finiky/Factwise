abbc44b420b80990bfdb86246c34b6e27ab26c9d
Akshay <98738875+finiky@users.noreply.github.com>
2023-09-05 17:08:09 +0530

    index.js to create a get request for sending the celebrities.json data as a response. installed express and cors and nodemon for server side, server was created to listen on port 5000

--- server/index.js
+++ server/index.js
@@ -1,2 +1,16 @@
-const data = require("./celebrities.json");
 const express = require("express");
+const app = express();
+const cors = require("cors");
+const data = require("./celebrities.json");
+const port = 5000;
+//middlewares
+app.use(cors());
+app.use(express.json());
+//get route for getting celebrities
+app.get("/celebrities", (request, response) => {
+  response.status(200).json(data);
+});
+
+app.listen(port, () => {
+  console.log("server started on port: " + port);
+});
