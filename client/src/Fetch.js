cfa083206b57a10ec1c560f62957003b689dd966
Akshay <98738875+finiky@users.noreply.github.com>
2023-09-06 01:05:21 +0530

    implemented the open collapse mechanism to display each celebrity in detail

--- client/src/Fetch.js
+++ client/src/Fetch.js
@@ -2,12 +2,15 @@ import { useState, useEffect } from "react";
 import styles from "./Fetch.module.css";
 import OpenCollapseButton from "./OpenCollapseButton";
 const Fetch = () => {
-  const [celebrities, setcelebrities] = useState([]);
+  const [celebrities, setCelebrities] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch("http://localhost:5000/celebrities");
       const data = await response.json();
-      setcelebrities(data);
+      const updatedData = data.map((celebrity) => {
+        return { ...celebrity, status: "+" };
+      });
+      setCelebrities(updatedData);
     };
     fetchData();
   }, []);
@@ -18,8 +21,24 @@ const Fetch = () => {
           <li className={styles.celebrity} key={celebrity.id}>
             <div className={styles.nameState}>
               <p>{`${celebrity.first} ${celebrity.last}`}</p>
-              <OpenCollapseButton id={celebrity.id} />
+              <OpenCollapseButton
+                celebrityId={celebrity.id}
+                celebrities={celebrities}
+                setCelebrities={setCelebrities}
+                status={celebrity.status}
+              />
             </div>
+            {celebrities.length === 0 ? (
+              <></>
+            ) : celebrity.status === "-" ? (
+              <div>
+                <p>{celebrity.gender}</p>
+                <p>{celebrity.country}</p>
+                <p>{celebrity.description}</p>
+              </div>
+            ) : (
+              <></>
+            )}
           </li>
         ))}
       </ul>
--- client/src/OpenCollapseButton.js
+++ client/src/OpenCollapseButton.js
@@ -1,20 +1,35 @@
-import { useState } from "react";
-const OpenCollapseButton = ({ id }) => {
-  const [state, setState] = useState("+");
+const OpenCollapseButton = ({
+  celebrityId,
+  celebrities,
+  setCelebrities,
+  status,
+}) => {
   const handleState = (e) => {
     e.preventDefault();
-    if (state === "+") {
-      setState("-");
-    }
-    if (state === "-") {
-      setState("+");
+    if (status === "+") {
+      const changeStatus = () => {
+        const modifiedCelebrities = celebrities.map((celebrity) => {
+          if (celebrity.id === celebrityId) {
+            celebrity.status = "-";
+          } else {
+            celebrity.status = "+";
+          }
+          return celebrity;
+        });
+        setCelebrities(modifiedCelebrities);
+      };
+      changeStatus();
+    } else if (status === "-") {
+      const changeStatus = () => {
+        const modifiedCelebrities = celebrities.map((celebrity) => {
+          return { ...celebrity, status: "+" };
+        });
+        setCelebrities(modifiedCelebrities);
+      };
+      changeStatus();
     }
   };
-  return (
-    <button data-state={state} id={id} onClick={handleState}>
-      {state}
-    </button>
-  );
+  return <button onClick={handleState}>{status}</button>;
 };
 
 export default OpenCollapseButton;
