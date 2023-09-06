3401b08b0d869f71d724293b45e3ff430703eca3
Akshay <98738875+finiky@users.noreply.github.com>
2023-09-06 16:00:47 +0530

    added prompt for confirming th delete in handleDelete function

--- client/src/handleDelete.js
+++ client/src/handleDelete.js
@@ -1,5 +1,7 @@
 const handleDelete = (e, celebrities, setCelebrities) => {
+  if (prompt("Are you sure you want to delete?") !== null) {
     const modifiedList = celebrities.toSpliced(e.target.dataset.index, 1);
     setCelebrities(modifiedList);
+  }
 };
 export default handleDelete;
