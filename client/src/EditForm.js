7d5500c03e35fceaad6835c5e681c49994568143
Akshay <98738875+finiky@users.noreply.github.com>
2023-09-06 23:02:52 +0530

    added boxsizing borderbox to the universal selector

--- client/src/EditForm.js
+++ client/src/EditForm.js
@@ -33,64 +33,69 @@ const EditForm = ({
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="dob">DOB</label>
-      <input
-        id="dob"
-        type="date"
-        value={celebrity.dob}
-        onChange={(e) => {
-          setCelebrity({ ...currentCelebrity, dob: e.target.value });
-        }}
-        required
-      />
-      <label htmlFor="gender">Gender</label>
-      <select
-        name="gender"
-        id="gender"
-        value={celebrity.gender}
-        onChange={(e) => {
-          setCelebrity({
-            ...celebrity,
-            gender: e.target.value,
-          });
-        }}
-        required
-      >
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-        <option value="transgender">Transgender</option>
-        <option value="rather not say">Rather not say</option>
-        <option value="other">Other</option>
-      </select>
-      <label htmlFor="country">Country</label>
-      <input
-        id="country"
-        type="text"
-        value={celebrity.country}
-        onChange={(e) => {
-          setCelebrity({ ...currentCelebrity, country: e.target.value });
-        }}
-        required
-      />
-      <label htmlFor="description">Description</label>
-      <textarea
-        rows="8"
-        cols="50"
-        id="description"
-        value={celebrity.description}
-        onChange={(e) => {
-          setCelebrity({ ...currentCelebrity, description: e.target.value });
-        }}
-        required
-      />
-      <button type="submit" disabled={enable}>
-        Save
-      </button>
-      <button type="button" onClick={handleCancel}>
-        Cancel
-      </button>
-    </form>
+    <div>
+      <h3>
+        {currentCelebrity.first} {currentCelebrity.last}
+      </h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="dob">DOB</label>
+        <input
+          id="dob"
+          type="date"
+          value={celebrity.dob}
+          onChange={(e) => {
+            setCelebrity({ ...currentCelebrity, dob: e.target.value });
+          }}
+          required
+        />
+        <label htmlFor="gender">Gender</label>
+        <select
+          name="gender"
+          id="gender"
+          value={celebrity.gender}
+          onChange={(e) => {
+            setCelebrity({
+              ...celebrity,
+              gender: e.target.value,
+            });
+          }}
+          required
+        >
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="transgender">Transgender</option>
+          <option value="rather not say">Rather not say</option>
+          <option value="other">Other</option>
+        </select>
+        <label htmlFor="country">Country</label>
+        <input
+          id="country"
+          type="text"
+          value={celebrity.country}
+          onChange={(e) => {
+            setCelebrity({ ...currentCelebrity, country: e.target.value });
+          }}
+          required
+        />
+        <label htmlFor="description">Description</label>
+        <textarea
+          rows="8"
+          cols="50"
+          id="description"
+          value={celebrity.description}
+          onChange={(e) => {
+            setCelebrity({ ...currentCelebrity, description: e.target.value });
+          }}
+          required
+        />
+        <button type="submit" disabled={enable}>
+          Save
+        </button>
+        <button type="button" onClick={handleCancel}>
+          Cancel
+        </button>
+      </form>
+    </div>
   );
 };
 
